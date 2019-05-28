09fed43da23aaa2a481185cfb4bd6e375cba6a19
Fosso Arcel <kraulain@gmail.com>
2019-05-28 21:36:21 +0100

    wire up all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeadBarComponent } from './head-bar/head-bar.component';
+import { FootBarComponent } from './foot-bar/foot-bar.component';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadBarComponent,
+    FootBarComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule
