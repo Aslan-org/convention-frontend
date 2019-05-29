c8c37b5f83a0c1ca0c250d1de5be71809973ac20
debenick17 <debenick17@gmail.com>
2019-05-29 03:35:22 +0100

    #0 | ectract each section into their own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,25 @@ import { AppComponent } from './app.component';
 import { HeadBarComponent } from './head-bar/head-bar.component';
 import { FootBarComponent } from './foot-bar/foot-bar.component';
 import { LoadingComponent } from './loading/loading.component';
+import { AboutComponent } from './about/about.component';
+import { SpeakersComponent } from './speakers/speakers.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+import { BlogComponent } from './blog/blog.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadBarComponent,
     FootBarComponent,
-    LoadingComponent
+    LoadingComponent,
+    AboutComponent,
+    SpeakersComponent,
+    SponsorsComponent,
+    BlogComponent,
+    RegisterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
